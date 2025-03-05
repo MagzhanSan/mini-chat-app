1664263012d829cfd616d386dbe5f56ffa0aa1e2
Magzhan <genshiprod@gmail.com>
2025-03-05 11:51:17 +0500

    up

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,24 @@
 import ChatList from "@/app/components/chat-list";
+import ChatWindow from "@/app/components/chat-window";
 
-export default async function HomePage({}: {
+export default async function HomePage({
+  searchParams,
+}: {
   searchParams: Promise<{ chatId?: string }>;
 }) {
-  // const parsedChatId = params.chatId ? Number(params.chatId) : null;
+  const params = await searchParams;
+  const parsedChatId = params.chatId ? Number(params.chatId) : null;
 
   return (
     <div className="flex h-screen">
       <ChatList />
 
       <div className="flex-1">
-        {/* {parsedChatId ? (
+        {parsedChatId ? (
           <ChatWindow chatId={parsedChatId} />
         ) : (
           <div className="p-4">Select a chat to view messages</div>
-        )} */}
+        )}
       </div>
     </div>
   );
